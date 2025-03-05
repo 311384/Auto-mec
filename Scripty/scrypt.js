ad524f78357ed382a9fe58f3619962318ddf46b0
311384 <nil7626@gmail.com>
2025-03-05 19:42:48 -0300

    stilo novo
    no if e no else

--- Scripty/scrypt.js
+++ Scripty/scrypt.js
@@ -1,17 +1,20 @@
 function adicionarTarefa(){
      //recebe valor do input do usuário e remove espaços em branco pelo comando .trim()
-    let inputTarefa = document.getElementById("inputTarefa");
+    const inputTarefa = document.getElementById("inputTarefa");
     let tarefa = inputTarefa.value.trim();
+    const mensagem = document.getElementById("mensagem");
     // se o usuário não digitar nada, exibe mensagem de erro
     if (tarefa == ""){
         let mensagemErro = "Por favor, insira uma tarefa válida!";
-        document.getElementById("mensagem").textContent = mensagemErro;    
+        mensagem.textContent = mensagemErro;
+        document.getElementById("mensagem").style.color ='#a34743';   
     } else { 
         // se o usuário digitar algo, exibe mensagem de sucesso       
         let mensagemSucesso = "Tarefa adicionada com sucesso!";
-        document.getElementById("mensagem").textContent = mensagemSucesso;
+        mensagem.textContent = mensagemSucesso;
+        document.getElementById("mensagem").style.color ='#28a745';
         //cria nova tarefa na lista li e insere na lista de ul
-    let listaTarefas = document.getElementById("listaTarefas")
+    const listaTarefas = document.getElementById("listaTarefas")
     let novaTarefa= document.createElement("li");
     novaTarefa.textContent = tarefa;
     listaTarefas.appendChild(novaTarefa);
