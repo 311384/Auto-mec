a17aa50859aef1c25c6abb970bd2ecea0637a48c
311384 <nil7626@gmail.com>
2025-03-05 19:08:33 -0300

    adicionado
    cod if e else

--- Scripty/scrypt.js
+++ Scripty/scrypt.js
@@ -1,13 +1,23 @@
 function adicionarTarefa(){
-    let mensagem = "Tarefa adicionada com sucesso!";  
+     //recebe valor do input do usuário e remove espaços em branco pelo comando .trim()
     let inputTarefa = document.getElementById("inputTarefa");
-    let tarefa = inputTarefa.value;
-    document.getElementById("mensagem").textContent = mensagem;
+    let tarefa = inputTarefa.value.trim();
+    // se o usuário não digitar nada, exibe mensagem de erro
+    if (tarefa == ""){
+        let mensagemErro = "Por favor, insira uma tarefa válida!";
+        document.getElementById("mensagem").textContent = mensagemErro;    
+    } else { 
+        // se o usuário digitar algo, exibe mensagem de sucesso       
+        let mensagemSucesso = "Tarefa adicionada com sucesso!";
+        document.getElementById("mensagem").textContent = mensagemSucesso;
+        //cria nova tarefa na lista li e insere na lista de ul
     let listaTarefas = document.getElementById("listaTarefas")
     let novaTarefa= document.createElement("li");
-
     novaTarefa.textContent = tarefa;
-    listaTarefas.appendChild(novaTarefa)
+    listaTarefas.appendChild(novaTarefa);
+    }
 
+    //limpa o input após adicionar a tarefa
 inputTarefa.value = "";
+
 }
