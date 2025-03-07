9d02ee9e16b80eccd62118ee651cce903e34b84e
311384 <nil7626@gmail.com>
2025-03-07 08:43:06 -0300

    adicionado arry
    JS

--- Scripty/scrypt.js
+++ Scripty/scrypt.js
@@ -1,26 +1,38 @@
-function adicionarTarefa(){
-     //recebe valor do input do usuário e remove espaços em branco pelo comando .trim()
+let tarefas = []
+
+function adicionarTarefa(){    
+     
     const inputTarefa = document.getElementById("inputTarefa");
     let tarefa = inputTarefa.value.trim();
     const mensagem = document.getElementById("mensagem");
-    // se o usuário não digitar nada, exibe mensagem de erro
+
     if (tarefa == ""){
         let mensagemErro = "Por favor, insira uma tarefa válida!";
         mensagem.textContent = mensagemErro;
         document.getElementById("mensagem").style.color ='#a34743';   
-    } else { 
-        // se o usuário digitar algo, exibe mensagem de sucesso       
+    } else {               
         let mensagemSucesso = "Tarefa adicionada com sucesso!";
         mensagem.textContent = mensagemSucesso;
         document.getElementById("mensagem").style.color ='#28a745';
-        //cria nova tarefa na lista li e insere na lista de ul
-    const listaTarefas = document.getElementById("listaTarefas")
-    let novaTarefa= document.createElement("li");
-    novaTarefa.textContent = tarefa;
-    listaTarefas.appendChild(novaTarefa);
-    }
-
-    //limpa o input após adicionar a tarefa
+       tarefas.push(tarefa) 
+       renderizarTarefas()
+    
+    
 inputTarefa.value = "";
+}
 
 }
+function renderizarTarefas() {
+    const listaTarefas = document.getElementById("listaTarefas")
+    listaTarefas.innerHTML=""
+            let i =0
+        for (i; i < tarefas.length; i++){
+            let novaTarefa= document.createElement("li")
+            novaTarefa.textContent = tarefas[i]
+            listaTarefas.appendChild(novaTarefa)
+         }
+        }
+
+    
+
+
